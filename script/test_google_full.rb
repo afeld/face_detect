aad6052f10741f864b5189989611ef86df6f63f9
Aidan Feldman <aidan.feldman@gsa.gov>
2016-05-01 14:00:47 -0400

    create a separate test script to get results directly from google api

--- script/test_google_full.rb
+++ script/test_google_full.rb
@@ -1,13 +1,15 @@
 # After following the installation steps in the README, run
 #
-#   bundle exec ruby script/test_google.rb
+#   bundle exec ruby script/test_google_full.rb
+#
+# You can also specify the IMG environment variable as a path to an image if you want to test with something else.
 #
 require 'pp'
 
 require_relative '../lib/face_detect'
 require_relative '../lib/face_detect/adapter/google'
 
-path = File.expand_path('../spec/fixtures/obama.jpg', __dir__)
+path = ENV['IMG'] || File.expand_path('../spec/fixtures/obama.jpg', __dir__)
 input = File.new(path)
 detector = FaceDetect.new(
   file: input,
---
+++ script/test_google_service.rb
@@ -0,0 +1,18 @@
+# After following the installation steps in the README, run
+#
+#   bundle exec ruby script/test_google_adatper.rb
+#
+# You can also specify the IMG environment variable as a path to an image if you want to test with something else.
+#
+require 'json'
+
+require_relative '../lib/face_detect'
+require_relative '../lib/face_detect/adapter/google'
+
+path = ENV['IMG'] || File.expand_path('../spec/fixtures/obama.jpg', __dir__)
+input = File.new(path)
+
+authorization = FaceDetect::Adapter::Google::AuthHelper.authorization
+service = FaceDetect::Adapter::Google::Service.new(authorization, input)
+result = service.run
+puts JSON.pretty_generate(result.to_h)
